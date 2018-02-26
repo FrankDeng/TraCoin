e525be38c372330e6dde0fa16bae55f2cc931cd8
tian-gao <tian.gao@columbia.edu>
2018-02-25 22:21:14 -0500

    Fix gdax auth bug and init api

---
+++ api/gdax/gdax.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+from logger import logger
+
+from api.api_class import Api
+from api.gdax.gdax_auth import GDAX_AUTH
+
+
+class GDAXApi(Api):
+    def __init__(self):
+        pass
